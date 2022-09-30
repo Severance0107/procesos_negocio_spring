831e2b6229577783b22c1eef1bd855f1c256ee5b
Sergio Ortiz <saortizi@ufpso.edu.co>
2022-09-29 21:03:55 -0500

    Validaciones de persistencia

--- src/main/java/com/procesos/negocios/sergio/models/Usuario.java
+++ src/main/java/com/procesos/negocios/sergio/models/Usuario.java
@@ -15,11 +15,16 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100, nullable = false)
     private String nombre;
+    @Column(length = 300, nullable = false)
     private String apellido;
+    @Column(length = 20, nullable = false)
     private String documento;
+    @Column(length = 100)
     private String direccion;
     private Date fechaNacimiento;
+    @Column(length = 20)
     private String telefono;
 
 }
